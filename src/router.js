ad8a628e791ce681addff2bdf4eb9962051cd947
jacksontate <jackson_tate@outlook.com>
2023-01-23 22:03:57 -0600

    this is a working local version

--- src/router.js
+++ src/router.js
@@ -2,8 +2,9 @@ import Vue from "vue";
 import Router from "vue-router";
 Vue.use(Router);
 
+// deploy test #4
 export default new Router({
-  base: process.env.NODE_ENV === 'development' ? "http://localhost/" : "/performance-t5/",
+  base: process.env.NODE_ENV === 'development' ? "http://localhost/" : "/performance-t5",
   mode: "history",
   routes: [
     {

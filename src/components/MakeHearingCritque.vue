ac8b9b0a74c9193e7843de04bc3f4d493c0c5c27
Kyler Seal <kyler.seal@eagles.oc.edu>
2023-02-13 13:47:05 -0600

    Start

---
+++ src/components/MakeHearingCritque.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="list-row">
+        <br>
+        <div class="col-md-1"></div>
+        <div class="col-md-8 sticky-top" style="margin: 10px; padding: 10px;">
+        <div class="input-group mb-3">
+            <input 
+            type="text"
+            class="form-control"
+            placeholder="search by name or course number"
+            /> <p></p>
+            <!--  <button @click="searchVal=''" style="margin-right: 10px; border-radius: 4px;" class="btn btn-dark">Clear Search</button>
+            <button @click="showModal(null)" style="margin-left: 10px; border-radius: 4px;" class="btn btn-dark">Add Course</button> -->
+
+        </div>
+        </div>
+    </div>
+</template>

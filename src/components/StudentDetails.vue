335a6fd76fa714fc04ad0a5b6220ea34da79a5c5
LucasEwald <42420052+LucasEwald@users.noreply.github.com>
2023-02-17 13:43:21 -0600

    framework

---
+++ src/components/StudentDetails.vue
@@ -0,0 +1,18 @@
+<template>
+    
+</template>
+<script>
+//imports
+
+export default {
+    name: 'student-details'
+    // ,data: {
+    // }
+    ,methods: {
+
+    }
+    ,mounted() {
+
+    }
+}
+</script>

80201f8f75367a6575740d42b98499a0e53805ef
JacobVollmering <jacob.vollmering@eagles.oc.edu>
2023-02-25 12:38:13 -0600

    Student list changes

--- src/components/Students.vue
+++ src/components/Students.vue
@@ -3,20 +3,28 @@
         <h1 class="mt-15 ml-16 text-blue" style="color:#03003f">Students</h1>
         <br>
         <v-card style="padding-left:30px; margin-left: 30px;" width="90%" class="center">
+            <v-row>
+                <v-col>Student ID</v-col>
+                <v-col>First Name</v-col>
+                <v-col>Last Name</v-col>
+            </v-row>
+        </v-card>
+        <v-card style="padding-left:30px; margin-left: 30px;" width="90%" class="center">
             <v-row
-                v-for="p in filteredPerformances"
-                :key="p.id"
-                @click="performanceDetails(p)"
+                v-for="u in users"
+                :key="u.id"
             >
-                <v-col>{{ p.event.type }}</v-col>
-                <v-col>{{ p.event.date }}</v-col>
-                <v-col>{{ p.accompanist }}</v-col>
+                <v-col>{{ u.id }}</v-col>
+                <v-col>{{ u.fName }}</v-col>
+                <v-col>{{ u.lName }}</v-col>
+                
             </v-row>
         </v-card>
     </div>
 </template>
 <script>
-import studentDS from '../services/StudentDataService';
+import userDS from '../services/UserDataService'
+//import studentDS from '../services/StudentDataService';
 import { useUserStore } from '@/stores/userStore';
 import { mapStores } from 'pinia';
 
@@ -27,15 +35,15 @@ export default{
     },
     data(){
         return{
-            students: [],
-            student: {}
+            users: [],
+            user: {}
         } 
     },
     mounted(){
-        studentDS.getAll()
+        userDS.getAll()
         .then(res =>{
-            res.data.forEach(s => this.students.push(s))
-            console.log(this.students)
+            res.data.filter(u => u.role == 'student').forEach(u => this.users.push(u))
+            console.log(this.users)
         })
         .catch(e => console.log(e))
     }

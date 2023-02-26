3ce3a3fc88727dfee58898bc270358ea4bd5d077
Jessica Longley <jessica.longley@gmail.com>
2023-02-26 15:58:42 -0600

    this version of dev is working right now

--- src/App.vue
+++ src/App.vue
@@ -63,8 +63,8 @@ import { mapStores } from "pinia";
             .then(res => {
                 this.users = res.data;
                 // this.user = this.users.find(u => u.id === 100); //David North Admin
-                // this.user = this.users.find(u => u.id === 200); //Kyle Pullen Faculty
-                this.user = this.users.find(u => u.id === 300); //Jess Long Student
+                this.user = this.users.find(u => u.id === 200); //Kyle Pullen Faculty
+                // this.user = this.users.find(u => u.id === 300); //Jess Long Student
 
                 this.userStore.setUser(this.user);
                 console.log(this.userStore);

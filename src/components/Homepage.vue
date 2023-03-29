575f1d5599f93a9cb094231b4a4febdc264fa1f6
Jessica Longley <jessica.longley@gmail.com>
2023-03-28 21:41:31 -0500

    vuetify cards arranged left to right

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,133 +1,65 @@
 <template>
   <div>
-    <!-- <div
-      class="col-md-15"
-      style="border-radius: 5px; padding: 10px; margin: 10px"
-    >
-      <div class="card">
-        <table class="table" style="margin-bottom: 0px">
-          <thead style="background-color: #f2f3f4">
-            <tr>
-              <th colspan="4" style="border-bottom: none">
-                <h4>Upcoming Events</h4>
-              </th>
-            </tr>
-            <tr>
-              <th scope="col">Name</th>
-              <th scope="col">Type</th>
-              <th scope="col">Date</th>
-              <th scope="col">Time</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              class="table-group-item"
-              v-for="e in upcomingEvents"
-              :key="e.id"
-              @click="signUp(e.id)"
-              v-show="upcomingEvents.length"
-            >
-              <td>{{ e.name }}</td>
-              <td>{{ e.type }}</td>
-              <td>{{ e.date }}</td>
-              <td>{{ e.startTime }} - {{ e.endTime }}</td>
-            </tr>
-            <tr v-show="!upcomingEvents.length">
-              <td><strong>No Upcoming Events</strong></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div> -->
-    <h1 class="mt-15 ml-16 text-blue" style="color:#03003f">Notifications</h1>
-    
-      <v-container class="mt-15">
+    <v-container>
+      <h1 class="mt-15 mb-4" style="color:#03003f">Notifications</h1>
+      
+      <v-slide-group show-arrows class="ml-n4">
+        <v-slide-item class="card-button align-center ml-4 mr-4" style="position:relative"
+            v-for="e in upcomingEvents"
+            :key="e.id"
+            @click="signUp(e.id)"
+            v-show="upcomingEvents.length"
+          >
+            <v-card max-width="500">
+              <v-card-title class="text-h4 font-weight-medium" style="color:#03003f">{{ e.name }}</v-card-title>
+              <v-card-subtitle style="color:#03003f">{{ e.date }}</v-card-subtitle>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="#03003f" elevation="5" style="color:#ffffff" @click="functionName()" :to="{name: 'all-events'}">
+                              View                              
+                            </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-slide-item>
+      </v-slide-group>
+
+    <h1 class="mt-15 mb-4" style="color:#03003f">Options</h1>
       <v-row class="align-center">
-        <v-col class="pa-5">
-          <!-- horizontal scroll not working -->
-          <horizontal-scroll>
-            <div class="card-button align-center" style="position:relative"
-              v-for="e in upcomingEvents"
-              :key="e.id"
-              @click="signUp(e.id)"
-              v-show="upcomingEvents.length"
-            >
-            <div>
-              <h2 class="pa-5" style="color:#03003f">{{ e.name }}             </h2>
-            </div>
-            <div>
-              <h2 class="pa-5" style="color:#03003f">{{ e.date }}             </h2>
-            </div>
-            <div class="align-end" style="position:absolute; bottom:20px; left:20px;">
+        <v-col>
+
+          <v-card class="card-button align-center" max-width="500">
+            <v-card-title class="text-h5 font-weight-medium" style="color:#03003f">View all events</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
               <v-btn color="#03003f" elevation="5" style="color:#ffffff" :to="{name: 'all-events'}">
-                View                              
-              </v-btn>
-            </div>
-        </div>
-          </horizontal-scroll>
-
+                Events                               
+                          </v-btn>
+            </v-card-actions>
+          </v-card>
         </v-col>
-
-      </v-row>
-    </v-container>
-
-
-    <h1 class="mt-15 ml-16 text-blue" style="color:#03003f">Options</h1>
-    <v-container class="mt-15">
-      <v-row class="align-center">
-        <v-col class="pa-5">
-          <div class="card-button align-center" style="position:relative">
-            <div>
-              <h2 class="pa-5" style="color:#03003f"> 
-                View all events
-              </h2>
-            </div>
-            <div class="align-end" style="position:absolute; bottom:20px; left:20px;">
-              <v-btn color="#03003f"
-                elevation="5"
-                style="color:#ffffff"
-                :to="{name: 'all-events'}"
-                >
-                Events                              
-              </v-btn>
-            </div>
-          </div>
+        <v-col  v-show="userStore.isAdmin">
+          <v-card class="card-button align-center" max-width="500">
+            <v-card-title class="text-h5 font-weight-medium" style="color:#03003f">Create a new event-details</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="#03003f" elevation="5" style="color:#ffffff" :to="{name: 'new-event'}">
+                Create                               
+                          </v-btn>
+            </v-card-actions>
+          </v-card>
         </v-col>
-        <v-col class="pa-5" v-show="userStore.isAdmin">
-          <div class="card-button align-center" style="position:relative">
-            <div>
-              <h2 class="pa-5" style="color:#03003f">
-                Create a new event
-              </h2>
-            </div>
-            <div class="align-end" style="position:absolute; bottom:20px; left:20px">
-              <v-btn color="#03003f"
-                elevation="5"
-                style="color:#ffffff"
-                :to="{name: 'new-event'}"
-                >
-                Create                              
-              </v-btn>
-            </div>
-          </div>
-        </v-col>
-        <v-col class="pa-5">
-          <div class="card-button align-center" style="position:relative">
-            <div>
-              <h2 class="pa-5" style="color:#03003f">
-                Sign up for an event
-              </h2>
-            </div>
-            <div class="align-end" style="position:absolute; bottom:20px; left:20px">
-              <v-btn color="#03003f"
-                elevation="5"
-                style="color:#ffffff"
-                :to="{name: 'all-events', query: {'upcoming': 1}}"
-                >
-                Sign up                              
-              </v-btn>
-            </div>
-          </div>
+        <v-col >
+          <v-card class="card-button align-center" max-width="500">
+            <v-card-title class="text-h5 font-weight-medium" style="color:#03003f">Sign up for an event</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="#03003f" elevation="5" style="color:#ffffff"                 
+              :to="{name: 'all-events', query: {'upcoming': 1}}"
+              >
+                Sign Up                               
+                          </v-btn>
+            </v-card-actions>
+          </v-card>
         </v-col>
       </v-row>
     </v-container>
@@ -140,7 +72,7 @@ import pds from "../services/PerformanceDataService";
 import sds from "../services/StudentDataService";
 import { useUserStore } from "@/stores/userStore";
 import { mapStores } from "pinia";
-// import "@kouts/vue-modal/dist/vue-modal.css";
+//import "@kouts/vue-modal/dist/vue-modal.css";
 
 export default {
   name: "home-page",
@@ -281,7 +213,6 @@ export default {
   border-radius: 4px;
   border-style: solid;
   border-color:#070243;
-  min-height: 300px;
   max-width: 350px;
   
 }

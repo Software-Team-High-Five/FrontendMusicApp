24260d7c0a96808a8a33b1b86937cdb73d719352
Jackson Tate <94014313+jacksontat3@users.noreply.github.com>
2023-04-11 00:19:06 -0500

    studnet edit and user list working

--- src/components/StudentEdit.vue
+++ src/components/StudentEdit.vue
@@ -68,36 +68,50 @@
             </v-col>
         </v-row>
         <!-- Instructor -->
-        <v-row>
-            <v-col class="text-left py-0" align-self="center" cols="4">
-                <strong>Instructor</strong>
+        <v-row v-for="(instrument, index) in studentInstruments" :key="index" v-if="!instrument.delete">
+            <v-col>
+                <v-row>
+                    <v-col class="text-left py-0" align-self="center" cols="4">
+                        <strong>Instrument: </strong>
+                    </v-col>
+                    <v-col class="py-0" cols="8" justify-content="center">
+                        <v-select
+                            v-if="instrument.isNew"
+                            v-model="instrument.instrumentId"
+                            :items="allInstruments"
+                            item-text="instrument"
+                            item-value="id"
+                            dense
+                        ></v-select>    
+                        <p style="margin-bottom: 0px;" v-if="!instrument.isNew">{{ instrument.instrumentName }}</p>
+                    </v-col>
+                </v-row>
             </v-col>
-            <v-col class="py-0" cols="8">
-                <v-select
-                    v-model="student.instructorId"
-                    :items="allInstructors"
-                    item-text="name"
-                    item-value="id"
-                    dense
-                ></v-select>
+            <v-col>
+                <v-row>
+                    <v-col class="text-left py-0" align-self="center" cols="4">
+                        <strong>Instructor: </strong>
+                    </v-col>
+                    <v-col class="py-0" cols="8">
+                        <v-select
+                            v-model="instrument.instructorId"
+                            :items="allInstructors"
+                            item-text="name"
+                            item-value="id"
+                            @change="instrument.instructorUpdated = true"
+                            dense
+                        ></v-select>
+                    </v-col>
+                </v-row>
             </v-col>
+            <v-col cols="1">
+                <v-btn icon small class="mr-2" @click="removeInstrument(instrument)"><v-icon small>mdi-delete</v-icon></v-btn>
+            </v-col>
+        </v-row> 
+        <v-row class="mx-12">
+            <v-btn @click="addNewInstrument()">Add Instrument</v-btn>
         </v-row>
-        <!-- Instruments -->
-        <v-row>
-            <v-col class="text-left py-0" align-self="center" cols="4">
-                <strong>Instruments</strong>
-            </v-col>
-            <v-col class="py-0" cols="8">
-                <v-select
-                    v-model="student.instrumentIds"
-                    :items="allInstruments"
-                    item-text="instrument"
-                    item-value="id"
-                    multiple
-                    dense
-                ></v-select>
-            </v-col>
-        </v-row>
+        <br><br>
         <v-row class="mx-12">
             <v-btn @click="Save">
                 Save Changes
@@ -106,11 +120,13 @@
     </v-container>
 </template>
 <script>
+import Vue from 'vue';
 import { useUserStore } from '@/stores/userStore';
 import { mapStores } from 'pinia';
 import userDS from '@/services/UserDataService';
 import instrumentDS from '@/services/InstrumentDataService';
 import studentDS from '@/services/StudentDataService';
+import user_instrumentDS from '@/services/User_InstrumentDataService';
 
 export default {
     name: 'student-edit'
@@ -119,8 +135,15 @@ export default {
             student: {}
             ,allInstructors: []
             ,allInstruments: []
-
+            ,studentInstruments: []
             ,loaded: false
+            ,newInstrument: {
+                instrumentId: null
+                ,instrumentName: ''
+                ,instructorId: null
+                ,instructorName: ''
+                ,isNew: true
+            }
         };
     }
     ,computed: {
@@ -135,40 +158,60 @@ export default {
                     ,classification: this.student.classification
                     ,semester: this.student.semester
                     ,level: this.student.level
-                    ,instructorId: this.student.instructorId
                 })
-                .then(() => {
-                    console.log('Student updated');
+                .then((res) => {
+                    console.log('Student updated', res);
                 })
                 .catch(e => {
                     console.log(e);
                 });
-            // Remove instruments that the student no longer plays
-            let instToRemove = this.student.prevInstruments.filter(i => !this.student.instrumentIds.includes(i));
-            for (const inst of instToRemove) {
-                await userDS.removeInstrument(this.student.id, inst)
-                    .then(() => {
-                        console.log(`${this.allInstruments.find(i => i.id == inst).instrument} removed from ${this.student.name}`);
-                    })
-                    .catch(e => {
-                        console.log(e);
-                    });
+            let instructorUpdates = this.studentInstruments.filter(i => !i.isNew && i.instructorUpdated);
+            for(const update of instructorUpdates) {
+                await user_instrumentDS.update({
+                    userId: this.student.id
+                    ,instrumentId: update.instrumentId
+                    ,instructorId: update.instructorId || null
+                })
+                    .then(res => console.log(res))
+                    .catch(e => console.log(e || 'unknown error updating'))
             }
-            // Add instruments that the student now plays
-            let instToAdd = this.student.instrumentIds.filter(i => !this.student.prevInstruments.includes(i));
-            for (const inst of instToAdd) {
-                await userDS.addInstrument(this.student.id, inst)
-                    .then(() => {
-                        console.log(`${this.allInstruments.find(i => i.id == inst).instrument} added to ${this.student.name}`);
-                    })
-                    .catch(e => {
-                        console.log(e);
-                    });
+            let deleteRecords = this.studentInstruments.filter(i => i.delete);
+            for(const del of deleteRecords) {
+                
+                await user_instrumentDS.delete(this.student.id, del.instrumentId)
+                    .then(res => console.log(res))
+                    .catch(e => console.log(e || 'unknow deleting'))
             }
-            // Refresh the cached list of instruments
-            this.student.prevInstruments = this.student.instrumentIds;
+            let newRecords = this.studentInstruments.filter(i => i.isNew);
 
-            if(this.userStore.isFaculty || this.userStore.isAdmin) await this.$router.push({name: 'user-list'})
+            for(const ad of newRecords){
+                console.log('creating: ', ad);
+                user_instrumentDS.create({
+                    userId: this.student.id
+                    ,instrumentId: ad.instrumentId
+                    ,instructorId: ad.instructorId || null
+                })
+                    .then(res => console.log(res))
+                    .catch(e => console.log(e || 'unknow creating'))
+            }
+
+            // if(this.userStore.isFaculty || this.userStore.isAdmin) await this.$router.push({name: 'user-list'})
+        }
+        ,removeInstrument(i) {
+            // Vue.delete(this.studentInstruments, i);
+            i.delete = true;
+            Vue.set(this.studentInstruments)
+        }
+        ,resetNewInstrument() {
+            this.newInstrument = {
+                instrumentId: null
+                ,instrumentName: ''
+                ,instructorId: null
+                ,instructorName: ''
+            }
+        }
+        ,addNewInstrument() {
+            this.studentInstruments.push({...this.newInstrument});
         }
     }
     ,async mounted() {
@@ -183,9 +226,18 @@ export default {
                     ,classification: res.data.student.classification
                     ,semester: res.data.student.semester
                     ,level: res.data.student.level
-                    ,instructorId: res.data.student.instructorId
-                    ,instrumentIds: res.data.instruments.map(i => i.id)
+                    ,instructorIds: res.data.user_instruments.map(i => i.instructorId)
+                    ,instrumentIds: res.data.user_instruments.map(i => i.instrumentId)
                 };
+                res.data.user_instruments.forEach(ui => {
+                    this.studentInstruments.push({
+                        instrumentName: ui.instrument.instrument
+                        ,instructorName: ui.instructor ? `${ui.instructor.fName} ${ui.instructor.lName}` : 'No Instructor'
+                        ,instructorId: ui.instructorId ?? 0
+                        ,instrumentId: ui.instrument.id
+                        ,instructorUpdated: false
+                    });
+                })
                 this.student.prevInstruments = this.student.instrumentIds;
                 console.log(this.student)
             })
@@ -197,6 +249,7 @@ export default {
             .then(res => {
                 this.allInstructors = res.data.filter(u => u.roles.find(r => r.role == 'faculty'))
                     .map(u => { return { id: u.id, name: `${u.fName} ${u.lName}` } });
+                this.allInstructors.push({id: 0, name: 'No Instructor'})
             })
             .catch(e => {
                 console.log(e);
@@ -204,7 +257,10 @@ export default {
         // Get all instrument data
         let instrumentsPromise = instrumentDS.getAll()
             .then(res => {
-                this.allInstruments = res.data;
+                let activeInstruments = this.studentInstruments.map(i => i.instrumentId);
+                console.log(activeInstruments);
+                this.allInstruments = res.data.filter(i => !activeInstruments.includes(i.id))
+                // this.allInstruments = res.data.filter(i => !this.student.prevInstruments.includes(i.id));
             })
             .catch(e => {
                 console.log(e);

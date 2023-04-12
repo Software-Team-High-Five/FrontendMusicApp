b06f9d63eff0f9c50cd6328c522870463a6268a4
LucasEwald <42420052+LucasEwald@users.noreply.github.com>
2023-04-12 13:09:36 -0500

    feat(allEvents): Updated allEvents look and routes
    Uses vuetify tables now
    Replaced MultiSelect component with a vuetify select
    Updated routes when clicking on events
    Added an all past events table for admins

--- src/components/AllEvents.vue
+++ src/components/AllEvents.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
     <v-container>
-      <link rel="stylesheet" href="https://unpkg.com/vue-multiselect@2.1.6/dist/vue-multiselect.min.css" />
-      <br />
       <!-- Header, Search, & Filter -->
       <v-row class="mt-15">
         <v-col class="col-md-3 sticky" v-if="!isUpcoming">
@@ -38,179 +36,93 @@
         </v-col>
       </v-row>
       <br />
+      
       <!-- Todays Events-->
-      <div
-        class="col-md-15"
-        style="border-radius: 5px; padding: 10px; margin: 10px"
-        v-show="!isUpcoming && todaysEvents.length"
-      >
-        <div class="card">
-          <table class="table" style="margin-bottom: 0px">
-            <thead style="background-color: #f2f3f4">
-              <tr>
-                <th colspan="4" style="border-bottom: none">
-                  <h4>Todays Events For Critiques</h4>
-                </th>
-              </tr>
-              <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Type</th>
-                <th scope="col">Date</th>
-                <th scope="col">Time</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                class="table-group-item"
-                v-for="e in todaysEvents"
-                :key="e.id"
-                @click="viewPerformance(e.id)"
-              >
-                <td>{{ e.name }}</td>
-                <td>{{ e.type }}</td>
-                <td>{{ e.date }}</td>
-                <td>{{ e.startTime }} - {{ e.endTime }}</td>
-              </tr>
-              <tr v-show="!myPastEvents.length">
-                <td><strong>No Events Today</strong></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+      <div v-if="!userStore.isStudent">
+        <v-card>
+          <v-card-title style="background-color:#f2f3f4">
+            Todays Events for Critiques
+          </v-card-title>
+          <v-data-table
+            :headers="headers"
+            :items="todaysEvents"
+            disable-pagination
+            hide-default-footer
+            no-data-text="No Events Scheduled For Today"
+            @click:row="e => viewPerformance(e.id)"
+          >
+          </v-data-table>
+        </v-card>
+        <br />
       </div>
+      
+      <!-- Future Events (unregistered) -->
+      <v-card>
+        <v-card-title style="background-color:#f2f3f4">
+          Future Events
+        </v-card-title>
+        <v-data-table
+          :headers="headers"
+          :items="FutureEvents"
+          disable-pagination
+          hide-default-footer
+          no-data-text="No Future Events Available"
+          @click:row="e => goFutureEvent(e)"
+        >
+        </v-data-table>
+      </v-card>
       <br />
-      <!-- Future Events -->
-      <div
-        class="col-md-15"
-        style="border-radius: 5px; padding: 10px; margin: 10px"
-      >
-        <div class="card">
-          <table class="table" style="margin-bottom: 0px">
-            <thead style="background-color: #f2f3f4">
-              <tr>
-                <th colspan="4" style="border-bottom: none">
-                  <!-- upcoming events are called unregistered events now -->
-                  <h4>Unregistered Events</h4>
-                </th>
-              </tr>
-              <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Type</th>
-                <th scope="col">Date</th>
-                <th scope="col">Time</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                class="table-group-item"
-                v-for="e in upcomingEvents"
-                :key="e.id"
-                @click="signUp(e.id)"
-                v-show="upcomingEvents.length"
-              >
-                <td>{{ e.name }}</td>
-                <td>{{ e.type }}</td>
-                <td>{{ e.date }}</td>
-                <td>{{ e.startTime }} - {{ e.endTime }}</td>
-              </tr>
-              <tr v-show="!upcomingEvents.length">
-                <td><strong>No Upcoming Events</strong></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+      
+      <!-- Non-admin Tables -->
+      <div v-show="userStore.isFaculty || userStore.isAccompanist || userStore.isStudent">
+        <!-- My Upcoming Events -->
+        <v-card>
+          <v-card-title style="background-color:#f2f3f4">
+            My Upcoming Events
+          </v-card-title>
+          <v-data-table
+            :headers="headers"
+            :items="myUpcomingEvents"
+            disable-pagination
+            hide-default-footer
+            no-data-text="You Have No Upcoming Events"
+            @click:row="e => goMyUpcomingEvent(e)"
+          >
+          </v-data-table>
+        </v-card>
+        <br />
+
+        <!-- My Past Events -->
+        <v-card>
+          <v-card-title style="background-color:#f2f3f4">
+            My Past Events
+          </v-card-title>
+          <v-data-table
+            :headers="headers"
+            :items="myPastEvents"
+            disable-pagination
+            hide-default-footer
+            no-data-text="You Have No Past Events"
+            @click:row="e => goMyPastEvent(e)"
+          >
+          </v-data-table>
+        </v-card>
+        <br />
       </div>
-      <br />
-      <!-- My Upcomming Events -->
-      <div
-        class="col-md-15"
-        style="border-radius: 5px; padding: 10px; margin: 10px"
-        v-show="!isUpcoming"
-      >
-        <div class="card">
-          <table class="table" style="margin-bottom: 0px">
-            <thead style="background-color: #f2f3f4">
-              <tr>
-                <th colspan="4" style="border-bottom: none">
-                  <h4>Registered Events</h4>
-                </th>
-              </tr>
-              <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Type</th>
-                <th scope="col">Date</th>
-                <th scope="col">Time</th>
-              </tr>
-            </thead>
-            <tbody>
-              <!--<tr
-                class="table-group-item"
-                v-for="e in registeredEvents"
-                :key="e.id"
-                @click="editSignup(e.id)"
-                v-show="!isUpcoming"
-              > -->
-              <tr
-                class="table-group-item"
-                v-for="e in registeredEvents"
-                :key="e.id"
-                @click="viewPerformance(e.id)"
-                v-show="!isUpcoming"
-              >
-                <td>{{ e.name }}</td>
-                <td>{{ e.type }}</td>
-                <td>{{ e.date }}</td>
-                <td>{{ e.startTime }} - {{ e.endTime }}</td>
-              </tr>
-              <tr v-show="!registeredEvents.length">
-                <td><strong>No Registered Events</strong></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-      <br />
-      <!-- My Past Events -->
-      <div
-        class="col-md-15"
-        style="border-radius: 5px; padding: 10px; margin: 10px"
-        v-show="!isUpcoming"
-      >
-        <div class="card">
-          <table class="table" style="margin-bottom: 0px">
-            <thead style="background-color: #f2f3f4">
-              <tr>
-                <th colspan="4" style="border-bottom: none">
-                  <h4>My Past Events</h4>
-                </th>
-              </tr>
-              <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Type</th>
-                <th scope="col">Date</th>
-                <th scope="col">Time</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                class="table-group-item"
-                v-for="e in myPastEvents"
-                :key="e.id"
-                @click="viewPerformance(e.id)"
-                v-show="myPastEvents.length"
-              >
-                <td>{{ e.name }}</td>
-                <td>{{ e.type }}</td>
-                <td>{{ e.date }}</td>
-                <td>{{ e.startTime }} - {{ e.endTime }}</td>
-              </tr>
-              <tr v-show="!myPastEvents.length">
-                <td><strong>No Past Events</strong></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
+      
+      <!-- All Past Events -->
+      <v-card v-if="userStore.isAdmin">
+        <v-card-title style="background-color:#f2f3f4">
+          All Past Events
+        </v-card-title>
+        <v-data-table
+          :headers="headers"
+          :items="allPastEvents"
+          no-data-text="No Past Events Available"
+          @click:row="e => goAllPast(e)"
+        >
+        </v-data-table>
+      </v-card>
     </v-container>
 
     <!-- Filter Popup -->
@@ -274,6 +186,7 @@ export default {
   name: "all-events",
   data() {
     return {
+      headers: [{ text:"Name", value:"name"},{ text:"Type", value:"type"},{ text:"Date", value:"date"},{ text:"Time", value:"timeRange"}],
       events: [],
       userPerformances: [],
       today: null,
@@ -295,6 +208,7 @@ export default {
       await eds.getAll()
         .then(res => {
           res.data.forEach(e => {
+            e.timeRange = `${e.startTime} - ${e.endTime}`;
             this.events.push(e);
           });
           console.log(this.events);
@@ -323,38 +237,58 @@ export default {
           .catch(e => console.log(e));
       }
 
-    }
-    ,signUp(eid) {
-      if(this.userStore.isAdmin){
-        console.log('direct admin to edit the upcoming event');
-      } else {
-        this.$router.push({name: 'sign-up', params: {eventId: eid}});
-      }
-    }
-    ,editSignup(eid){
-      this.$router.push({name: 'sign-up', params: { eventId: eid }, query: { 'editing': 1 }})
-    }
-    ,viewPerformance(eid){
-      if(this.userStore.user.role == 'student'){
-        const viewPerformance = this.userPerformances.find(p => p.eventId == eid);
-        this.$router.push({ name: 'view-performance', params: {performanceId: viewPerformance.id}});
-      } else {
-        this.$router.push({ name: 'event-details', params: {eventId: eid}});
-      }
-    }
-    ,generalFilter(event) {
+    },
+    goFutureEvent(event) {
+      if (this.userStore.isAdmin)
+        this.$router.push({ name: 'event-details-admin', params: { eventId: event.id }});
+      else
+        this.$router.push({ name: 'sign-up', params: { eventId: event.id } })
+    },
+    goMyUpcomingEvent(event) {
+      if (this.userStore.isStudent || !event.openForSignup)
+        this.$router.push({ name: 'sign-up', params: { eventId: event.id }, query: { 'editing': 1 }});
+      else
+        this.$router.push({ name: 'event-details', params: { eventId: event.id }});
+    },
+    goMyPastEvent(event) {
+      if (this.userStore.isStudent)
+        this.$router.push({ name: 'view-performance', params: {
+          performanceId: this.userPerformances.find(p => p.eventId === event.id).id }});
+      else
+        this.$router.push({ name: 'event-details', params: { eventId: event.id }});
+    },
+    goAllPast(event) {
+      this.$router.push({ name: 'event-details-admin', params: { eventId: event.id }});
+    },
+    generalFilter(event) {
       // Filter by search string
       if(this.searchString.length > 0 && event.name.toLowerCase().indexOf(this.searchString.toLowerCase()) == -1)
         return false;
       // Filter by selected event types
       return this.activeTypes.length == 0 || this.activeTypes.includes(event.type);
-    }
-    ,clearFilters() {
+    },
+    clearFilters() {
       this.activeTypes = [];
     }
   },
   computed: {
     ...mapStores(useUserStore),
+    FutureEvents() {
+      return this.events.filter((e) =>
+        e.date > this.today &&
+        !this.userPerformances.some((p) => p.eventId == e.id) &&
+        !e.availabilities.some(a => a.userId == this.userStore.user.id) &&
+        (!this.userStore.isStudent || e.openForSignup) &&
+        this.generalFilter(e)            
+      );
+    },
+    myUpcomingEvents() {
+      return this.events.filter((e) =>
+        e.date > this.today &&
+        this.generalFilter(e) &&
+        (this.userPerformances.find((p) => p.eventId == e.id) || (e.availabilities.find(a => a.userId == this.userStore.user.id)))
+      );
+    },
     myPastEvents() {
       return this.events.filter((e) =>
         e.date < this.today &&
@@ -362,19 +296,10 @@ export default {
         this.generalFilter(e)
       );
     },
-    upcomingEvents() {
+    allPastEvents() {
       return this.events.filter((e) =>
-        e.date > this.today &&
-        !this.userPerformances.find((p) => p.eventId == e.id) && 
-        !e.availabilities.find(a => a.userId == this.userStore.user.id) && 
-        this.generalFilter(e)            
-      );
-    },
-    registeredEvents() {
-      return this.events.filter((e) =>
-        e.date > this.today &&
-        this.generalFilter(e) &&
-        (this.userPerformances.find((p) => p.eventId == e.id) || (e.availabilities.find(a => a.userId == this.userStore.user.id)))
+        e.date < this.today &&
+        this.generalFilter(e)
       );
     },
     todaysEvents() {

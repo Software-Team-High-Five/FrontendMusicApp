abd4343f35da077ab7f73a1c9575be3c945cd654
Jackson Tate <94014313+jacksontat3@users.noreply.github.com>
2023-04-12 14:11:46 -0500

    sign up changes

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,8 +1,8 @@
 <template>
     <v-container>
-        <v-row>
+        <v-row v-if="loaded">
             <v-col>
-                <h1 class="text-center">Sign Up Page</h1>
+                <h1 class="text-center">{{ this.userStore.isStudent ? 'Sign Up ' : 'Mark Availability ' }} for "{{ event.name }}""</h1>
             </v-col>
         </v-row>
         <div v-if="loaded">
@@ -14,43 +14,27 @@
                     <v-col style="width:min-content">
                         <div><span class="font-weight-medium">Type:</span> {{ event.type }}</div>
                         <div><span class="font-weight-medium">Date:</span> {{ event.date }}</div>
+                        <div><span class="font-weight-medium">Time:</span> {{ displayTime(event.startTime) }} - {{ displayTime(event.endTime) }}</div>
+
                     </v-col>
                 </v-row>
                 <v-row>
-                    <v-col>
-                        <!-- Select a time slot -->
-                        <h4 class="text-center">Select a time slot</h4>
-                        <v-card>
-                            <v-calendar
-                                type="day"
-                                v-model="event.date"
-                                :events="timeSlots"
-                                interval-minutes="5"
-                                :first-interval="calAtts.fIndex"
-                                :interval-count="calAtts.iCount"
-                                :short-intervals="false"
-                                @click:event="selectTimeSlot"
-                                :event-color="getEventColor"
-                                hide-header
-                                style="max-height:600px"
-                            >
-                                <template v-slot:event="{ event, eventParsed }">
-                                    <div class="pl-1 text-center">
-                                        <strong>{{ event.name }}</strong><br>
-                                        {{ timeRange(eventParsed.start, eventParsed.end) }}
-                                    </div>
-                                </template>
-                            </v-calendar>
-                        </v-card>
-                        <v-row v-if="!userStore.isStudent" :align="'center'" style="padding-top: 15px;">
-                            <button class="btn btn-dark" @click="facultySubmit">Submit</button>
-                        </v-row>
-                    </v-col>    
                     <v-col v-if="userStore.isStudent">
                         <br>
                         <v-container>
-                            <!-- Add an accompanist to the performance -->
+                            <!-- Select the instrument -->
                             <v-row>
+                                <v-select
+                                    label="Instrument"
+                                    v-model="selectedInstrumentId"
+                                    :items="instruments"
+                                    item-text="instrument"
+                                    item-value="id"
+                                    @change="assignInstructor()"
+                                ></v-select>
+                            </v-row>
+                            <!-- Add an accompanist to the performance -->
+                            <v-row style="max-width: 500px;">
                                 <v-col>
                                     <v-select
                                         :disabled="noAccompanist"
@@ -59,6 +43,7 @@
                                         item-text="fullName"
                                         item-value="id"
                                         label="Accompanist"
+                                        style="max"
                                     ></v-select>
                                 </v-col>
                                 <v-col cols="1">
@@ -69,16 +54,7 @@
                                     ></v-checkbox>
                                 </v-col>
                             </v-row>
-                            <!-- Select the instrument -->
-                            <v-row>
-                                <v-select
-                                    label="Instrument"
-                                    v-model="selectedInstrumentId"
-                                    :items="instruments"
-                                    item-text="instrument"
-                                    item-value="id"
-                                ></v-select>
-                            </v-row>
+    
                             <!-- Add songs to the performance -->
                             <v-row>
                                 <v-card>
@@ -99,13 +75,65 @@
                                 </v-card>
                             </v-row>
                             <!-- Submit -->
+                            <br><br>
                             <v-row>
-                                <v-btn @click="studentSubmit">
+                                <v-btn 
+                                    @click="studentSubmit" 
+                                    color="#03003f" 
+                                    style="color: white;">
                                     Submit
                                 </v-btn>
                             </v-row>
                         </v-container>
                     </v-col>
+                    <v-col>
+                        <v-row>
+                            <v-col cols="2" justify-content="center">
+                                <v-checkbox
+                                        v-if="!this.userStore.isStudent"
+                                        style="float: left;"
+                                        v-model="selectAll"
+                                        label="Select All"
+                                        @click="allTimeSlots()"
+                                ></v-checkbox>
+                            </v-col>
+                            <!-- Select a time slot -->
+                            <v-col>
+                                <h4 class="text-center">{{this.userStore.isStudent ? 'Select a time slot' : 'Select time slots'}}</h4>
+                            </v-col>
+                        </v-row>
+                        <!-- <v-card> -->
+                            <v-calendar
+                                type="day"
+                                v-model="event.date"
+                                :events="timeSlots"
+                                interval-minutes="5"
+                                :first-interval="calAtts.fIndex"
+                                :interval-count="calAtts.iCount"
+                                :short-intervals="false"
+                                @click:event="selectTimeSlot"
+                                :event-color="getEventColor"
+                                hide-header
+                                style="max-height:600px"
+                            >
+                                <template v-slot:event="{ event, eventParsed }">
+                                    <div class="pl-1 text-center">
+                                        <strong>{{ event.name }}</strong><br>
+                                        {{ timeRange(eventParsed.start, eventParsed.end) }}
+                                    </div>
+                                </template>
+                            </v-calendar>
+                        <!-- </v-card> -->
+                        <br>
+                        <v-row v-if="!userStore.isStudent" :align="'center'" style="padding-top: 15px;">
+                            <v-btn 
+                                color="#03003f" 
+                                @click="facultySubmit"
+                                style="color: white">
+                                Submit
+                            </v-btn>
+                        </v-row>
+                    </v-col>    
                 </v-row>
             </div>
             <!-- Error while Loading -->
@@ -167,6 +195,7 @@ export default {
             ,availabilities: {}
             ,person1TimeSlots: []
             ,person2TimeSlots: []
+            ,selectAll: false
         }
     }
     ,computed: {
@@ -221,10 +250,8 @@ export default {
                             name: `Time slot ${count}`
                             ,start: `${this.event.date}T${Math.floor(startI / 12).toString().padStart(2, '0')}:${(startI % 12 * 5).toString().padStart(2, '0')}`
                             ,end: `${this.event.date}T${Math.floor((startI + 1) / 12).toString().padStart(2, '0')}:${((startI + 1) % 12 * 5).toString().padStart(2, '0')}`
-                            // ,instructorAvailabile: false
-                            // ,accompanistAvailable: false
-                            // ,available: (this.instructorAvailabile && this.accompanistAvailable) || (this.instructorAvailabile && Object.keys(this.availabilities).length == 1)  //whether the timeslot is open or not
                             ,available: true
+                            ,openForSelect: this.userStore.isStudent ? (this.selectedInstrumentId && ( this.selectedAccompanist || this.noAccompanist )) : true
                         });
                         startI++;
                         count++;
@@ -236,10 +263,10 @@ export default {
                 });
 
             // Get user instrument list
-            this.userStore.user.instruments.forEach(i => {
-                this.instruments.push(i);
-            })
-            
+            this.userStore.user.user_instruments.forEach(i => {
+                i.instrument.instructorId = i.instructorId
+                this.instruments.push(i.instrument);
+            })            
             
             // Get user song list
             if(this.userStore.isStudent){
@@ -289,11 +316,6 @@ export default {
                 this.takenTimes = this.takenTimes.map(t => t.available = true);
             }
 
-            //get instructor availability
-            if(this.userStore.isStudent) {
-                this.getEventAvailability(this.userStore.user.student.instructorId);
-            }
-
             //set up for edit
             if(this.$route.query.editing && this.userStore.isStudent){
                 await PerformanceDS.editPerformance(this.userStore.user.student.id, this.$route.params.eventId)
@@ -311,16 +333,15 @@ export default {
             }
             this.loaded = true;
         }
-
         // Other Methods
         ,getEventColor(event) {
+            this.userStore.isStudent ? event.openForSelect = (this.selectedInstrumentId && (this.selectedAccompanist || this.noAccompanist)) : true
             if (this.selectedTime && this.selectedTime.name === event.name)
-                return "secondary";
-            if (!event.available)
-                return "grey"
+                return "#03003f";
+            if (!event.available || !event.openForSelect)
+                return "grey";
             return "primary";
         }
-
         // will be used for both faculty and accompanists, uid will be the user id of either an instructor or an accompanist
         ,async getEventAvailability(uid) {
             await AvailabilityDS.getForEvent(uid, this.event.id)
@@ -346,7 +367,6 @@ export default {
                 // allows for faculty to select multiple time slots
                 } else {
                     timeSlot.available = false;
-                    console.log(timeSlot.name.split(" ").pop())
                     this.selectedTime[timeSlot.name.split(" ").pop()] = timeSlot;
                 }
             // support for removing time slots for faculty
@@ -514,7 +534,25 @@ export default {
             return timeSlots;
 
         }
-        
+        ,assignInstructor() {
+            //get instructor availability
+            if(this.userStore.isStudent) {
+                const instructorId = this.instruments.find(i => i.id == this.selectedInstrumentId).instructorId;
+                this.getEventAvailability(instructorId);
+            }
+            return false;
+        }
+        ,displayTime(time) {
+            const hours = (parseInt((time.substr(0, 2)) + 11) % 12 + 1);
+            const min = time.substr(3, 2);
+            return hours + ':' + min + (time.substr(0, 2) > 11 ? ' PM' : ' AM');
+        }   
+        ,allTimeSlots() {
+            this.timeSlots.forEach(ts => {
+                this.selectTimeSlot({event: ts});
+            });
+
+        }     
     }
     ,mounted() {
         this.initializeData();
